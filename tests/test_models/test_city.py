ffc6b565891dd6b86c644e5f497c53de714aa6bd
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-19 13:11:12 +0200

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,40 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Unittest module for the City class"""
+
+import unittest
 from models.city import City
+from models.state import State
+from models.base_model import BaseModel
 
 
-class test_City(test_basemodel):
-    """ """
+class TestCity(unittest.TestCase):
+    """Test case for the City class"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "City"
-        self.value = City
+    def setUp(self):
+        """Set up the test environment"""
+        pass
+
+    def tearDown(self):
+        """Clean up the test environment"""
+        pass
 
     def test_state_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.state_id), str)
+        """Test the state_id attribute of City"""
+        new_city = City()
+        self.assertIsInstance(new_city.state_id, str)
 
     def test_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        """Test the name attribute of City"""
+        new_city = City()
+        self.assertIsInstance(new_city.name, str)
+
+    def test_inheritance(self):
+        """Test inheritance of City"""
+        new_city = City()
+        self.assertIsInstance(new_city, BaseModel)
+        self.assertIsInstance(new_city, City)
+        self.assertFalse(type(new_city) == State)
+
+
+if __name__ == '__main__':
+    unittest.main()
