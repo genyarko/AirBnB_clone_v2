1d368c910309a6ad3823ca4b350f1627dc7fd0ef
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-19 13:08:15 +0200

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-""" """
+"""Unittest module for the Amenity class"""
+
+import unittest
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
-class test_Amenity(test_basemodel):
-    """ """
+class TestAmenity(test_basemodel):
+    """Test case for the Amenity class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
-    def test_name2(self):
-        """ """
+    def test_name(self):
+        """Test the name attribute"""
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
