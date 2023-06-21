6641ddcbff632f008efbdae5a9848554858d082b
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-21 18:40:40 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -19,24 +19,39 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
 
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
 
-    '''amenities for DBStorage'''
-    amenities = relationship('Amenity', backref='place_amenities',
-                             secondary=place_amenity, viewonly=False)
+        '''amenities for DBStorage'''
+        amenities = relationship('Amenity', backref='place_amenities',
+                                 secondary=place_amenity, viewonly=False)
 
-    '''reviews for DBStorage'''
-    reviews = relationship('Review', backref='place',
-                           cascade='all, delete, delete-orphan')
+        '''reviews for DBStorage'''
+        reviews = relationship('Review', backref='place',
+                               cascade='all, delete, delete-orphan')
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+        '''amenities for FileStorage'''
         @property
         def amenities(self):
             '''
@@ -75,3 +90,4 @@ class Place(BaseModel, Base):
             result = storage.all(Review)
             return [review_obj for review_obj in result.values()
                     if self.id == review_obj.place_id]
+
