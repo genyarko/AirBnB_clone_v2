5b7abbb2e8a8f20d7b4dc5b26fa3c76505f1b751
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-21 18:36:46 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -21,4 +21,19 @@ class City(BaseModel, Base):
         name = ''
 
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargsclass City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
+    else:
+        state_id = ''
+        name = ''
+
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
