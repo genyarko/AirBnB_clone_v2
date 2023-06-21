3c16473e7242a35068cd6aff72d4baf21baac26a
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-21 12:07:17 +0200

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,5 @@
-#!/bin/bash
-
-# Create database if it doesn't exist
-mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS hbnb_dev_db;"
-
-# Create user if it doesn't exist
-mysql -u root -p -e "CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';"
-
-# Grant all privileges to the user on hbnb_dev_db
-mysql -u root -p -e "GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';"
-
-# Grant SELECT privilege on performance_schema to the user
-mysql -u root -p -e "GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';"
+-- CREATES A DATABASE AND A USER
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
