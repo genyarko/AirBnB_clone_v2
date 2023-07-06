300012daacf877a33ca8583d4328960dcacfc940
genyarko <115112347+genyarko@users.noreply.github.com>
2023-07-06 22:31:45 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,53 @@
 #!/usr/bin/python3
 """
 Fabric script to deploy tgz archive
-fab -f 2-do_deploy_web_static.py do_deploy:archive_path=filepath
-    -i private-key -u user
 """
-
-from os.path import exists
 from fabric.api import put, run, env
+from os.path import exists
 
-env.hosts = ['35.243.128.200', '3.239.120.96']
-
+# Set the web server IP addresses
+env.hosts = ['	100.24.206.17', '54.173.234.218']
 
 def do_deploy(archive_path):
-    """
-    copies archive file from local to my webservers
-    """
+    """Distributes an archive to the web servers."""
 
+    # Check if the archive file exists
     if not exists(archive_path):
         return False
+
     try:
-        file_name = archive_path.split("/")[-1].split(".")[0]
-        put(archive_path, "/tmp/")
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
 
-        run("mkdir -p /data/web_static/releases/{}".format(file_name))
+        # Get the filename without extension
+        archive_filename = archive_path.split('/')[-1].split('.')[0]
 
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-            .format(file_name, file_name))
+        # Create the release directory
+        run('mkdir -p /data/web_static/releases/{}/'.format(archive_filename))
 
-        run('rm -rf /tmp/{}.tgz'.format(file_name))
+        # Uncompress the archive to the release directory
+        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
+            .format(archive_filename, archive_filename))
 
-        run(('mv /data/web_static/releases/{}/web_static/* ' +
-            '/data/web_static/releases/{}/')
-            .format(file_name, file_name))
+        # Delete the archive from the web server
+        run('rm /tmp/{}.tgz'.format(archive_filename))
 
+        # Move the files from the web_static directory to the release directory
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'
+            .format(archive_filename, archive_filename))
+
+        # Remove the now empty web_static directory
         run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(file_name))
+            .format(archive_filename))
 
+        # Delete the symbolic link /data/web_static/current
         run('rm -rf /data/web_static/current')
 
-        run(('ln -s /data/web_static/releases/{}/' +
-            ' /data/web_static/current')
-            .format(file_name))
+        # Create a new symbolic link
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(archive_filename))
+
         return True
-    except Exception:
+    except Exception as e:
+        print(str(e))
         return False
