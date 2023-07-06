7d951ed1e039ffc0327fb02510f4ea718bf75532
genyarko <115112347+genyarko@users.noreply.github.com>
2023-07-06 22:47:31 +0200

    Create 101-setup_web_static.pp

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,81 @@
+# Configures a web server for deployment of web_static.
+# Configures a web server for deployment of web_static.
+$nginx_conf = "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 https://th3-gr00t.tk;
+    }
+
+    error_page 404 /404.html;
+
+    location /404 {
+        root /var/www/html;
+        internal;
+    }
+}
+"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+file { '/data':
+  ensure  => 'directory'
+} ->
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+file { '/var/www':
+  ensure => 'directory'
+} ->
+file { '/var/www/html':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+} ->
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf,
+  notify  => Exec['nginx restart']
+} ->
+exec { 'nginx restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+  refreshonly => true
+}
