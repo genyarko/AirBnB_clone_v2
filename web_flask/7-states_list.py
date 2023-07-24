11a59cd1296b51ff872fecba37f1eb3f7a454363
genyarko <115112347+genyarko@users.noreply.github.com>
2023-07-24 03:49:15 +0200

    Add files via upload

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application
-"""
+"""Flask web application"""
 
-from flask import Flask, render_template
 from models import storage
+from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
-def teardown_session(exception):
-    """
-    After each request, remove the current SQLAlchemy Session
-    """
+def tearItDown(exception):
+    """Reload the current SQLAlchemy session"""
     storage.close()
 
+
 @app.route('/states_list', strict_slashes=False)
-def states_list():
-    """
-    Route: /states_list
-    Returns:
-        HTML page with a list of all State objects present in DBStorage sorted by name
-    """
-    states = storage.all("State").values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+def list_states():
+    """/states_list route"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run('0.0.0.0', 5000)
