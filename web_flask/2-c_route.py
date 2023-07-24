3a4256e72942f5d67bc5778ae4f586456e281dda
genyarko <115112347+genyarko@users.noreply.github.com>
2023-07-24 03:15:53 +0200

    Create 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Route: /
+    Returns:
+        "Hello HBNB!"
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route: /hbnb
+    Returns:
+        "HBNB"
+    """
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Route: /c/<text>
+    Args:
+        text: URL parameter representing the text variable
+    Returns:
+        "C " followed by the value of the text variable (replace underscores with spaces)
+    """
+    return 'C {}'.format(escape(text.replace('_', ' ')))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
