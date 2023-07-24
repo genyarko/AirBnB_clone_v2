b6698442aac4ba863b22f278bf40d79d78e7e891
genyarko <115112347+genyarko@users.noreply.github.com>
2023-07-24 03:08:23 +0200

    Create 0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Route for the home page
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
