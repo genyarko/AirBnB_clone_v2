af4d1ef058473480b34dfbd56cfe1c80dd997c98
genyarko <115112347+genyarko@users.noreply.github.com>
2023-07-24 03:53:29 +0200

    Add files via upload

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,36 +1,26 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application
-"""
+"""Flask web application"""
 
-from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
+from flask import Flask, render_template
+from os import getenv
 
 app = Flask(__name__)
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def list_states():
+    """/cities_by_states route"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
 @app.teardown_appcontext
-def teardown_session(exception):
-    """
-    After each request, remove the current SQLAlchemy Session
-    """
+def teardown(exception):
+    """Reload the current SQLAlchemy session"""
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """
-    Route: /cities_by_states
-    Returns:
-        HTML page with a list of all State objects present in DBStorage sorted by name
-        and for each state, a list of City objects linked to that state, sorted by name.
-    """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=sorted_states)
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run('0.0.0.0', 5000)
